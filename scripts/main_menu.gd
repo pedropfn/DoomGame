9498c549979f51850f51e334881d6065167bfe50
Pedro F Neto <pfn.pedrofneto@gmail.com>
2026-03-29 03:57:35 -0300

    feat: WIP settings config file

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,57 +1,58 @@
 extends Node2D
 
+@onready var mouseSense = GlobalSettings.MOUSE_SENS
+@onready var renderQuality = GlobalSettings.RENDER_QUALITY
+
 func _ready() -> void:
 	%fullscreen.button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
 	%vsync.button_pressed = true if DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED else false
+	# %ScaleQuality.value = renderQuality
+
 	%MainVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
 	%SfxVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("sfx")))
 	%BgmVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("bgm")))
+	
+	%MouseSenseLabel.text = "MouseSense: " + str(mouseSense)
+	%MouseSense.value = mouseSense
+	
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/intro.tscn")
-
 
 func _on_settings_pressed() -> void:
 	%MainButtons.visible = false
 	%SettingsMenu.visible = true
 
-
 func _on_credits_pressed() -> void:
 	%MainButtons.visible = false
 	%CreditsMenu.visible = true
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_back_pressed() -> void:
 	%MainButtons.visible = true
 	%SettingsMenu.visible = false
 	%CreditsMenu.visible = false
 
-
 func _on_fullscreen_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
-
-
-func _on_main_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
-
-
-func _on_sfx_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("sfx"), value)
-
-
-func _on_bgm_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("bgm"), value)
-
+	GlobalSettings.fullscreen_toggled(toggled_on)
 
 func _on_vsync_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
-	else:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	GlobalSettings.vsync_toggled(toggled_on)
+
+func _on_scale_quality_item_selected(index: int) -> void:
+	GlobalSettings.scale_quality_item_selected(index)
+
+func _on_main_volume_value_changed(value: float) -> void:
+	GlobalSettings.main_volume_value_changed(value)
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	GlobalSettings.sfx_volume_value_changed(value)
+
+func _on_bgm_volume_value_changed(value: float) -> void:
+	GlobalSettings.bgm_volume_value_changed(value)
+
+func _on_mouse_sense_value_changed(value: float) -> void:
+	mouseSense = value
+	%MouseSenseLabel.text = "MouseSense: " + str(mouseSense)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
-var MOUSE_SENS = 0.5
-const SPEED = 5.0
+
+var speed = 5.0
 
 @onready var animatedSprite2d = $CanvasLayer/GunBase/AnimatedSprite2D
 @onready var rayCast3d = $RayCast3D
@@ -11,6 +11,8 @@ const SPEED = 5.0
 
 @onready var currentScene = get_tree().current_scene.name
 @onready var mainMenu = get_tree().get_first_node_in_group("MainMenu")
+@onready var mouseSense = GlobalSettings.MOUSE_SENS
+@onready var renderQuality = GlobalSettings.RENDER_QUALITY
 
 @onready var canShoot = true
 @onready var dead = false
@@ -30,15 +32,20 @@ func _ready():
 	
 	%fullscreen.button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN else false
 	%vsync.button_pressed = true if DisplayServer.window_get_vsync_mode() == DisplayServer.VSYNC_ENABLED else false
+	# %ScaleQuality.value = renderQuality
+
 	%MainVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
 	%SfxVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("sfx")))
 	%BgmVolume.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("bgm")))
+	
+	%MouseSenseLabel.text = "MouseSense: " + str(mouseSense)
+	%MouseSense.value = mouseSense
 
 func _input(event):
 	if dead:
 		return
 	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * MOUSE_SENS
+		rotation_degrees.y -= event.relative.x * mouseSense
 
 func _process(_delta):
 	if Input.is_action_just_pressed("exit"):
@@ -67,14 +74,14 @@ func _physics_process(_delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 		if !footstepSound.playing:
 			footstepSound.pitch_scale = randf_range(.8, 1.2)
 			footstepSound.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 		footstepSound.stop()
 	
 	move_and_slide()
@@ -144,35 +151,34 @@ func _on_back_pressed() -> void:
 	get_tree().paused = false
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
-func _on_fullscreen_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
-func _on_main_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
-
-func _on_sfx_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("sfx"), value)
-
-func _on_bgm_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("bgm"), value)
-
-func _on_restart_settings_pressed() -> void:
-	restart()
+### Settings
 
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
+func _on_restart_settings_pressed() -> void:
+	restart()
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	GlobalSettings.fullscreen_toggled(toggled_on)
 
 func _on_vsync_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
-	else:
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+	GlobalSettings.vsync_toggled(toggled_on)
 
+func _on_scale_quality_item_selected(index: int) -> void:
+	GlobalSettings.scale_quality_item_selected(index)
+
+func _on_main_volume_value_changed(value: float) -> void:
+	GlobalSettings.main_volume_value_changed(value)
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	GlobalSettings.sfx_volume_value_changed(value)
+
+func _on_bgm_volume_value_changed(value: float) -> void:
+	GlobalSettings.bgm_volume_value_changed(value)
 
 func _on_mouse_sense_value_changed(value: float) -> void:
-	MOUSE_SENS = value
+	mouseSense = value
+	%MouseSenseLabel.text = "MouseSense: " + str(mouseSense)
